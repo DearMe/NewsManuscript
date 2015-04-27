1c6576a385c8b999930118dde57d12dc8e39f96c
DearMe <ruanqixiang@gmail.com>
2015-04-27 10:49:57 +0800

    20150427

--- NewsManuscript/src/com/test/Test.java
+++ NewsManuscript/src/com/test/Test.java
@@ -2,12 +2,13 @@ package com.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ApplicationContext ac = new ClassPathXmlApplicationContext("\\WEB-INF\\applicationContext.xml");
+		ApplicationContext ac = new FileSystemXmlApplicationContext("WebRoot/WEB-INF/applicationContext.xml");
 		TestService testService = (TestService)ac.getBean("testService");
 		System.out.println(testService.getName());
 	}
